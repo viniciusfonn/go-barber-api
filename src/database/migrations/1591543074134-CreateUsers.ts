88223465566f3324ef2839dc46f2c358b54ef5d3
vinicius fonseca <viniciusfon96@gmail.com>
2020-06-07 22:24:59 -0300

    added the appointments migration and added the some methods to create and show appointments with db integration

---
+++ src/database/migrations/1591543074134-CreateUsers.ts
@@ -0,0 +1,7 @@
+import { MigrationInterface, QueryRunner } from 'typeorm';
+
+export default class CreateUsers1591543074134 implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {}
+
+  public async down(queryRunner: QueryRunner): Promise<void> {}
+}
