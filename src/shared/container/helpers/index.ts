384d8821398591ef8f8d5b8c8eb6a4aabc0218da
vinicius <viniciusfon96@gmail.com>
2020-07-08 21:42:20 -0300

    added Handlebars Implementation

--- src/shared/container/helpers/index.ts
+++ src/shared/container/helpers/index.ts
@@ -6,6 +6,14 @@ import DiskStorageHelper from './StorageHelper/implementations/DiskStorageHelper
 import IMailHelper from './MailHelper/models/IMailHelper';
 import EtherealMailHelpe from './MailHelper/implementations/EtherealMailHelper';
 
+import IMailTemplateHelper from './MailTemplateHelper/models/IMailTemplateHelper';
+import handlebarsMailTemplateHelper from './MailTemplateHelper/implementations/HandlebarsMailTemplateHelper';
+
 container.registerSingleton<IStorageHelper>('StorageHelper', DiskStorageHelper);
 
 container.registerInstance<IMailHelper>('IMailHelper', new EtherealMailHelpe());
+
+container.registerSingleton<IMailTemplateHelper>(
+  'MailTemplateHelper',
+  handlebarsMailTemplateHelper,
+);
